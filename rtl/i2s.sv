61b2d7491d8248e7205f8c8665f317067e8a0a89
Tommaso Terzano <118925910+TommiTerza@users.noreply.github.com>
2026-09-17 12:55:12 +0200

    Added TX channel to transmit data (#4)
    feature(dev/tx): Added TX channel to transmit data; Disabled by default for retro-compatibility

--- rtl/i2s.sv
+++ rtl/i2s.sv
@@ -9,7 +9,8 @@
 
 module i2s #(
     parameter type reg_req_t = logic,
-    parameter type reg_rsp_t = logic
+    parameter type reg_rsp_t = logic,
+    parameter bit I2sDisableTx = 1'b1
 ) (
     input logic clk_i,
     input logic rst_ni,
@@ -25,15 +26,17 @@ module i2s #(
     output logic i2s_ws_o,
     output logic i2s_ws_oe_o,
     input  logic i2s_ws_i,
+
+    // Independent data lines for TX and RX to allow full duplex operation
     output logic i2s_sd_o,
-    output logic i2s_sd_oe_o,
     input  logic i2s_sd_i,
 
     // Interrupt
     output logic intr_i2s_event_o,
 
     // DMA signal
-    output logic i2s_rx_valid_o
+    output logic i2s_rx_valid_o,
+    output logic i2s_tx_ready_o
 );
 
   import i2s_reg_pkg::*;
@@ -50,6 +53,12 @@ module i2s #(
   logic data_rx_ready;
   logic data_rx_overflow;
 
+  logic [MaxWordWidth-1:0] data_tx;
+  logic data_tx_valid;
+  logic data_tx_ready;
+  logic data_tx_underflow;
+  logic data_tx_overflow;
+
   logic event_i2s_event;
 
   logic [$clog2(MaxWordWidth)-1:0] word_width;
@@ -62,14 +71,16 @@ module i2s #(
 
   // DMA signal
   assign i2s_rx_valid_o = data_rx_valid;
+  assign i2s_tx_ready_o = data_tx_ready;
 
   // STATUS signal
   assign hw2reg.status.rx_data_ready.d = data_rx_valid;
   assign hw2reg.status.rx_overflow.d = data_rx_overflow;
+  assign hw2reg.status.tx_ready.d = data_tx_ready;
+  assign hw2reg.status.tx_underflow.d = data_tx_underflow;
+  assign hw2reg.status.tx_overflow.d = data_tx_overflow;
 
   // IO
-  assign i2s_sd_oe_o = 1'b0;
-  assign i2s_sd_o = 1'b0;
   assign i2s_sck_oe_o = reg2hw.control.en_io.q;
   assign i2s_ws_oe_o = reg2hw.control.en_io.q;
   unread _sck_i (i2s_sck_i);
@@ -80,8 +91,25 @@ module i2s #(
   assign hw2reg.control.reset_watermark.d = 1'b0;
   assign hw2reg.control.reset_rx_overflow.de = ~data_rx_overflow;
   assign hw2reg.control.reset_rx_overflow.d = 1'b0;
+  assign hw2reg.control.reset_tx_underflow.de = ~data_tx_underflow;
+  assign hw2reg.control.reset_tx_underflow.d = 1'b0;
+  assign hw2reg.control.reset_tx_overflow.de = ~data_tx_overflow;
+  assign hw2reg.control.reset_tx_overflow.d = 1'b0;
 
+  assign data_tx = reg2hw.txdata.q;
+  assign data_tx_valid = reg2hw.txdata.qe;
 
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (~rst_ni) begin
+      data_tx_overflow <= 1'b0;
+    end else begin
+        if (reg2hw.control.reset_tx_overflow.q) begin
+            data_tx_overflow <= 1'b0;
+        end else if (reg2hw.txdata.qe && !data_tx_ready) begin
+            data_tx_overflow <= 1'b1;
+        end
+    end
+  end
 
   // Register logic
   i2s_reg_top #(
@@ -97,12 +125,11 @@ module i2s #(
       .devmode_i(1'b1)
   );
 
-
-
   // Core logic
   i2s_core #(
       .MaxWordWidth(MaxWordWidth),
-      .ClkDividerWidth(ClkDividerWidth)
+      .ClkDividerWidth(ClkDividerWidth),
+      .I2sDisableTx(I2sDisableTx)
   ) i2s_core_i (
       .clk_i(clk_i),
       .rst_ni(rst_ni),
@@ -110,9 +137,11 @@ module i2s #(
       .en_ws_i(reg2hw.control.en_ws.q),
       .en_rx_left_i(reg2hw.control.en_rx.q[0]),
       .en_rx_right_i(reg2hw.control.en_rx.q[1]),
+      .en_tx_i(reg2hw.control.en_tx.q & reg2hw.control.en_io.q),
 
       .sck_o(i2s_sck_o),
       .ws_o (i2s_ws_o),
+      .sd_o (i2s_sd_o),
       .sd_i (i2s_sd_i),
 
       .cfg_clock_div_i(reg2hw.clkdividx.q),
@@ -123,12 +152,17 @@ module i2s #(
       .data_rx_valid_o(data_rx_valid),
       .data_rx_ready_i(data_rx_ready),
 
-      .clear_rx_overflow_i(reg2hw.control.reset_rx_overflow.q),
+      .data_tx_i(data_tx),
+      .data_tx_valid_i(data_tx_valid),
+      .data_tx_ready_o(data_tx_ready),
+
+      .clear_rx_overflow_i (reg2hw.control.reset_rx_overflow.q),
+      .clear_tx_underflow_i(reg2hw.control.reset_tx_underflow.q),
 
       .running_o(hw2reg.status.running.d),
-      .data_rx_overflow_o(data_rx_overflow)
+      .data_rx_overflow_o(data_rx_overflow),
+      .data_tx_underflow_o(data_tx_underflow)
   );
-
 
   // watermark counter
   // count bus reads and trigger interrupt 
--- rtl/i2s_core.sv
+++ rtl/i2s_core.sv
@@ -8,7 +8,8 @@
 
 module i2s_core #(
     parameter MaxWordWidth = 32,
-    parameter ClkDividerWidth = 8
+    parameter ClkDividerWidth = 8,
+    parameter bit I2sDisableTx = 1'b1
 ) (
     input logic clk_i,
     input logic rst_ni,
@@ -16,10 +17,12 @@ module i2s_core #(
     input logic en_ws_i,
     input logic en_rx_left_i,
     input logic en_rx_right_i,
+    input logic en_tx_i,
 
     // IO interface
     output logic sck_o,
     output logic ws_o,
+    output logic sd_o,
     input  logic sd_i,
 
     // config
@@ -32,14 +35,21 @@ module i2s_core #(
     output logic                    data_rx_valid_o,
     input  logic                    data_rx_ready_i,
 
+    input  logic [MaxWordWidth-1:0] data_tx_i,
+    input  logic                    data_tx_valid_i,
+    output logic                    data_tx_ready_o,
+
     input logic clear_rx_overflow_i,
+    input logic clear_tx_underflow_i,
 
     output logic running_o,
-    output logic data_rx_overflow_o
+    output logic data_rx_overflow_o,
+    output logic data_tx_underflow_o
 );
 
   logic                    ws;
   logic                    sck;
+  logic                    sd;
 
   logic [MaxWordWidth-1:0] data_rx_dc;
   logic                    data_rx_dc_valid;
@@ -49,6 +59,7 @@ module i2s_core #(
 
   assign ws_o  = ws;
   assign sck_o = sck;
+  assign sd_o  = sd;
 
   clk_int_div #(
       .DIV_VALUE_WIDTH(ClkDividerWidth),
@@ -97,7 +108,7 @@ module i2s_core #(
 
   // cdc
   cdc_fifo_gray #(
-      .T(logic [31:0]),
+      .T(logic [MaxWordWidth-1:0]),
       .LOG_DEPTH(2)
   ) rx_cdc_i (
       .src_clk_i  (sck),
@@ -123,6 +134,65 @@ module i2s_core #(
       .serial_i(data_rx_overflow_async),
       .serial_o(data_rx_overflow_o)
   );
+
+  generate
+    if (!I2sDisableTx) begin : gen_i2s_tx
+      logic [MaxWordWidth-1:0] data_tx_dc;
+      logic                    data_tx_dc_valid;
+      logic                    data_tx_dc_ready;
+      logic                    data_tx_underflow_async;
+
+      i2s_tx_channel #(
+          .MaxWordWidth(MaxWordWidth)
+      ) i2s_tx_channel_i (
+          .sck_i (sck),
+          .rst_ni(rst_ni),
+          .en_i  (en_tx_i & en_ws_i),
+          .ws_i  (ws),
+
+          .word_width_i(cfg_word_width_i),
+
+          .data_i(data_tx_dc),
+          .data_valid_i(data_tx_dc_valid),
+          .data_ready_o(data_tx_dc_ready),
+
+          .sd_o(sd),
+          .underflow_o(data_tx_underflow_async),
+          .clear_underflow_i(clear_tx_underflow_i)
+      );
+
+      cdc_fifo_gray #(
+          .T(logic [MaxWordWidth-1:0]),
+          .LOG_DEPTH(2)
+      ) tx_cdc_i (
+          .src_clk_i  (clk_i),
+          .src_rst_ni (rst_ni),
+          .src_ready_o(data_tx_ready_o),
+          .src_data_i (data_tx_i),
+          .src_valid_i(data_tx_valid_i),
+
+          .dst_rst_ni (rst_ni),
+          .dst_clk_i  (sck),
+          .dst_data_o (data_tx_dc),
+          .dst_valid_o(data_tx_dc_valid),
+          .dst_ready_i(data_tx_dc_ready)
+      );
+
+      sync #(
+          .STAGES(2),
+          .ResetValue(1'b0)
+      ) data_tx_underflow_sync_i (
+          .clk_i,
+          .rst_ni,
+          .serial_i(data_tx_underflow_async),
+          .serial_o(data_tx_underflow_o)
+      );
+    end else begin : gen_no_i2s_tx
+      assign sd                  = 1'b0;
+      assign data_tx_ready_o     = 1'b0;
+      assign data_tx_underflow_o = 1'b0;
+    end
+  endgenerate
 
 
   logic en_q;
